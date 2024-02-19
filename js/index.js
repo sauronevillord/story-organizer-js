c12fb6ea2ebf01645b4667c00c84a8fa0ac0817e
redFox22 <marcolucibelli@gmail.com>
2024-02-19 08:43:36 +0100

    aggiunta card con immagine e textbox. Bottone per inserire l'immagine

--- js/index.js
+++ js/index.js
@@ -47,10 +47,39 @@ function selectEntry ( category_pos, entry_pos ) {
     let category = current_project.categories[category_pos];
 
     console.log(category);
-
+    
     let entry = category.entries[entry_pos];
 
-    entry.sections.forEach( (el) => {
-        console.log(el);
-    });
+    createCard(entry);
+
+
+    
+
+
+}
+
+function createCard(entry){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let cardContent = document.createElement('div');
+    cardContent.classList.add('card__content');
+    let cardTitle = document.createElement('h3');
+    cardTitle.textContent = entry.name;
+    let image = document.createElement('img');
+    image.src="./resources/images/how-to-draw-a-cartoon-monster-step-6-removebg-preview.png";
+    let btnChangeImg = document.createElement('input');
+    btnChangeImg.type = "file";
+    btnChangeImg.accept = "image/png, image/jpeg";
+    btnChangeImg.onchange = function(){
+        image.src = URL.createObjectURL(btnChangeImg.files[0]);
+    }
+    let textarea = document.createElement('textarea');
+    textarea.placeholder = "Inserisci il testo qui...";
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(image);
+    cardContent.appendChild(btnChangeImg);
+    cardContent.appendChild(textarea);
+    card.appendChild(cardContent);
+    right_container.appendChild(card);
+
 }
